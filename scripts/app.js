e198b3d9f952d92733f3b4828b2bda3893c1d548
Francisco Ciarallo <fran.jc02@gmail.com>
2024-10-20 22:25:04 -0300

    Carga de productos a partir de un json

--- scripts/app.js
+++ scripts/app.js
@@ -66,13 +66,76 @@ class Producto {
 
 }
 
-const productos = [];
+let productos = [];
+/*
 productos.push(new Producto(1, "Chocolate 40% cacao", 1500, 50, ["Manteca de cacao", "Miel organica", "Cacao"], "../images/chocolate-40.jpg", "info-producto-cacao-40.html"));
 productos.push(new Producto(2, "Chocolate 50% cacao", 1500, 50, ["Manteca de cacao", "Miel organica", "Cacao"], "../images/chocolate-50.jpg", "info-producto-cacao-50.html"));
 productos.push(new Producto(3, "Chocolate 60% cacao", 1500, 50, ["Manteca de cacao", "Miel organica", "Cacao"], "../images/chocolate-60.jpg", "info-producto-cacao-60.html"));
 productos.push(new Producto(4, "Chocolate 70% cacao", 1500, 50, ["Manteca de cacao", "Miel organica", "Cacao"], "../images/chocolate-70.jpg", "info-producto-cacao-70.html"));
 productos.push(new Producto(5, "Chocolate 80% cacao", 1500, 50, ["Manteca de cacao", "Miel organica", "Cacao"], "../images/chocolate-80.jpg", "info-producto-cacao-80.html"));
 productos.push(new Producto(6, "Chocolate 90% cacao", 1500, 50, ["Manteca de cacao", "Miel organica", "Cacao"], "../images/chocolate-90.jpg", "info-producto-cacao-90.html"));
+*/
+
+const renderizarProductos = (productos) => {
+    const productosContainer = document.getElementById("productos-container")
+    productos.forEach((producto) => {
+        const div = document.createElement("div")
+        div.classList.add("col-12")
+        div.classList.add("col-sm-4")
+        div.classList.add("col-md-3")
+        div.classList.add("d-flex")
+        div.classList.add("justify-content-center")
+    
+        div.innerHTML = `
+            <div class="producto-caja">
+                <a href="${producto.html}">
+                    <img class="productos-img" src="${producto.img}" alt="${producto.nombre}">
+                </a>
+                <a href="${producto.html}">
+                    <h2>${producto.nombre}</h2>
+                </a>
+                <button class="btn btn-primary comprar-producto-btn" type="button" name="${producto.id}">Añadir al carrito</button>
+            </div>
+        `
+        productosContainer.appendChild(div)
+    })
+
+    const anadirAlCarritoBtns = document.getElementsByClassName("comprar-producto-btn")
+    for (let btn of anadirAlCarritoBtns) {
+        btn.addEventListener("click", () => {
+            const productoOriginal = productos.find(producto => producto.id == btn.name);
+            productoSeleccionado = new Producto(productoOriginal.id, productoOriginal.nombre, productoOriginal.precio, productoOriginal.cantidad, productoOriginal.ingredientes, productoOriginal.img, productoOriginal.html)
+            productoSeleccionado.cantidad = 1
+            if(productoSeleccionado == null || productoSeleccionado.cantidad <= 0) {
+                alert("Producto no disponible")
+            }
+            else{
+                agregarProductoAlCarrito(productoSeleccionado, 1)
+            }
+        })
+    }
+}
+
+const cargarProductos = async (url) => {
+    try {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`ERROR: ${res.ok}`);
+        }
+        
+        const productosJson = await res.json();
+        console.log(productosJson);
+
+        productos = productosJson.map(producto => new Producto(producto.id, producto.nombre, producto.precio, producto.cantidad, producto.ingredientes, producto.img, producto.html));
+        console.log(productos);
+
+        renderizarProductos(productos);
+  
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+cargarProductos("../data/productos.json");
 
 const carrito = [];
 
@@ -190,41 +253,8 @@ function calcularMontoTotalCarrito() {
     return total.toFixed(2)
 }
 
-const productosContainer = document.getElementById("productos-container")
-productos.forEach((producto) => {
-    const div = document.createElement("div")
-    div.classList.add("col-12")
-    div.classList.add("col-sm-4")
-    div.classList.add("col-md-3")
-    div.classList.add("d-flex")
-    div.classList.add("justify-content-center")
-
-    div.innerHTML = `
-        <div class="producto-caja">
-            <a href="${producto.html}">
-                <img class="productos-img" src="${producto.img}" alt="${producto.nombre}">
-            </a>
-            <a href="${producto.html}">
-                <h2>${producto.nombre}</h2>
-            </a>
-            <button class="btn btn-primary comprar-producto-btn" type="button" name="${producto.id}">Añadir al carrito</button>
-        </div>
-    `
-    productosContainer.appendChild(div)
-})
 
 
-const anadirAlCarritoBtns = document.getElementsByClassName("comprar-producto-btn")
-for (let btn of anadirAlCarritoBtns) {
-    btn.addEventListener("click", () => {
-        const productoOriginal = productos.find(producto => producto.id == btn.name);
-        productoSeleccionado = new Producto(productoOriginal.id, productoOriginal.nombre, productoOriginal.precio, productoOriginal.cantidad, productoOriginal.ingredientes, productoOriginal.img, productoOriginal.html)
-        productoSeleccionado.cantidad = 1
-        if(productoSeleccionado == null || productoSeleccionado.cantidad <= 0) {
-            alert("Producto no disponible")
-        }
-        else{
-            agregarProductoAlCarrito(productoSeleccionado, 1)
-        }
-    })
-}
+
+
+
